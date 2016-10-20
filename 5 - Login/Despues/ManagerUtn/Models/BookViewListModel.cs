7a9178b56e86f8a4b18f295bd737444cfa1db417
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-10-19 22:25:31 -0300

    Se agrega Login

---
+++ 5 - Login/Despues/ManagerUtn/Models/BookViewListModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using ManagerUtn.Entities;
+
+namespace ManagerUtn.Models
+{
+    public class BookViewListModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Image { get; set; }
+        public string Descripcion { get; set; }
+        public int CountComentarios { get; set; } = 0;
+
+        public BookViewListModel()
+        {
+
+        }
+
+        public BookViewListModel(Book book)
+        {
+            this.Id = book.Id;
+            this.Titulo = book.Titulo;
+            this.Image = book.Image;
+            this.Descripcion = book.Descripcion;
+            if (book.Comentarios != null)
+                this.CountComentarios = book.Comentarios.Count();
+        }
+    }
+}

3b799701264be41e549c6d7c02de04dc7a87053f
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-10-20 11:48:42 -0300

    Se agrega data

--- 5 - Login/Despues/ManagerUtn/Migrations/Configuration.cs
+++ 5 - Login/Despues/ManagerUtn/Migrations/Configuration.cs
@@ -13,7 +13,7 @@ namespace ManagerUtn.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(ManagerUtn.Models.ApplicationDbContext context)
@@ -24,13 +24,25 @@ namespace ManagerUtn.Migrations
                 var roleManager = new RoleManager<IdentityRole>(roleStore);
                 var store = new UserStore<ApplicationUser>(context);
                 var manager = new UserManager<ApplicationUser>(store);
-                var user = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com"};
+                var user = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
 
                 manager.Create(user, "password");
 
-                roleManager.Create(new IdentityRole {Name = "Admin"});
+                roleManager.Create(new IdentityRole { Name = "Admin" });
                 manager.AddToRole(user.Id, "Admin");
             }
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            //  This method will be called after migrating to the latest version.
+
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
